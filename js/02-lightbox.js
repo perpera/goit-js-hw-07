713b876b6afe144cff55be54d59d261585e92ba2
perpera <131678085+perpera@users.noreply.github.com>
2023-09-03 15:26:21 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a></li>`;
+}
+
+const markup = galleryItems.map(createGalleryItem).join("");
+
+galleryList.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
